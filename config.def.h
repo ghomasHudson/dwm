ed03244261d5e1330e48599f4be3aa9bca243968
Thomas Hudson <g.t.hudson@durham.ac.uk>
2020-03-03 23:42:55 +0000

    Removed shift from fullscreen

--- config.def.h
+++ config.def.h
@@ -84,7 +84,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
+	{ MODKEY,             XK_f,      fullscreen,     {0} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
